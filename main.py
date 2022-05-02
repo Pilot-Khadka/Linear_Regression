33248d470b18cff00603a6ce1b20295466acc6e0
Pilot338 <46417154+Pilot-Khadka@users.noreply.github.com>
2022-05-02 11:29:53 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-#removing empty rows
+
+# removing empty rows
 def clean(df):
     nan_value = float("NaN")
     df.replace("", nan_value, inplace=True)
-    df.dropna(subset = ["y"], inplace=True)
-    
-def draw(df,str):
-    plt.figure(figsize = (10,10))
-    plt.title(str)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.scatter(df['x'],df['y'])
+    df.dropna(subset=["y"], inplace=True)
+
+
+def draw(df, string):
+    plt.figure(figsize=(10, 10))
+    plt.title(string, fontsize=16)
+    plt.xlabel("x", fontsize=16)
+    plt.ylabel("y", fontsize=16)
+    plt.scatter(df["x"], df["y"])
     plt.show()
-        
-train = pd.read_csv ('/Users/pilot/Desktop/archive/train.csv')
+
+
+# Training Data
+train = pd.read_csv("/Users/pilot/Desktop/archive/train.csv")
 clean(train)
-draw(train,"Train Data")
+# draw(train)
+X_train = train["x"].values.reshape(-1, 1)
+Y_train = train["y"].values.reshape(-1, 1)
 
-test = pd.read_csv('/Users/pilot/Desktop/archive/test.csv')
+# Testing Data
+test = pd.read_csv("/Users/pilot/Desktop/archive/test.csv")
 clean(test)
-draw(test,"Test Data")
-
-#make 2D for scikit-learn.LinearRegression
-X_train = train[['x']]
-X_test = test[['x']]
-
-Y_train = train['y']
-Y_test = test['y']
+# draw(test)
+X_test = test["x"].values.reshape(-1, 1)
+Y_test = test["y"].values.reshape(-1, 1)
 
 lin_reg = LinearRegression()
 lin_reg.fit(X_train, Y_train)
-accuracy = lin_reg.score(X_test,Y_test)
 
+# Plotting using scikit
+plt.figure(figsize=(10, 10))
+plt.scatter(X_train, Y_train, s=8)
+plt.plot(X_train, lin_reg.predict(X_train), color="orange")
+plt.title("Train Data", fontsize=20)
+plt.xlabel("x", fontsize=20)
+plt.ylabel("y", fontsize=20)
+plt.show()
+
+accuracy = lin_reg.score(X_test, Y_test)
 print(accuracy)
-
