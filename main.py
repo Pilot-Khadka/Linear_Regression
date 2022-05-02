ac84e30f836239b36854448d036148c6e1afc766
Pilot338 <46417154+Pilot-Khadka@users.noreply.github.com>
2022-05-02 10:12:56 +0545

    Update main.py
    made function to remove empty rows

--- main.py
+++ main.py
@@ -2,26 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
+#removing empty rows
+def clean(df):
+    nan_value = float("NaN")
+    df.replace("", nan_value, inplace=True)
+    df.dropna(subset = ["y"], inplace=True)
+        
 train = pd.read_csv ('/Users/pilot/Desktop/archive/train.csv')
-
-#removing empty,NaN rows
-nan_value = float("NaN")
-train.replace("", nan_value, inplace=True)
-train.dropna(subset = ["y"], inplace=True)
-
-X_train = train['x'].values.reshape(-1,1)
-Y_train = train['y'].values.reshape(-1,1)
-
-plt.figure(figsize = (10,10))
-plt.scatter(X_train,Y_train)
-plt.show()
+clean(train)
 
 test = pd.read_csv('/Users/pilot/Desktop/archive/test.csv')
-X_test = test['x'].values.reshape(-1,1)
-Y_test = test['x'].values.reshape(-1,1)
+clean(test)
+
+#make 2D
+X_train = train[['x']]
+X_test = test[['x']]
+
+Y_train = train['y']
+Y_test = test['y']
+
+plt.figure(figsize = (10,10))
+plt.xlabel("X_train")
+plt.ylabel("Y_train")
+plt.scatter(X_train,Y_train)
+plt.show()
 
 lin_reg = LinearRegression()
 lin_reg.fit(X_train, Y_train)
 accuracy = lin_reg.score(X_test,Y_test)
 
 print(accuracy)
+
