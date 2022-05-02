5f8737bd9d4b6e026aaefdd820ac8a649656bb7d
Pilot338 <46417154+Pilot-Khadka@users.noreply.github.com>
2022-05-02 10:28:45 +0545

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,29 @@ def clean(df):
     nan_value = float("NaN")
     df.replace("", nan_value, inplace=True)
     df.dropna(subset = ["y"], inplace=True)
+    
+def draw(df,str):
+    plt.figure(figsize = (10,10))
+    plt.title(str)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.scatter(df['x'],df['y'])
+    plt.show()
         
 train = pd.read_csv ('/Users/pilot/Desktop/archive/train.csv')
 clean(train)
+draw(train,"Train Data")
 
 test = pd.read_csv('/Users/pilot/Desktop/archive/test.csv')
 clean(test)
+draw(test,"Test Data")
 
-#make 2D
+#make 2D for scikit-learn.LinearRegression
 X_train = train[['x']]
 X_test = test[['x']]
 
 Y_train = train['y']
 Y_test = test['y']
-
-plt.figure(figsize = (10,10))
-plt.xlabel("X_train")
-plt.ylabel("Y_train")
-plt.scatter(X_train,Y_train)
-plt.show()
 
 lin_reg = LinearRegression()
 lin_reg.fit(X_train, Y_train)
